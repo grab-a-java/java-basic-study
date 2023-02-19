5f22a6f990659d9fad5139ccdbcba7d291d9e717
jongmee <ra123221@gmail.com>
2023-02-19 22:17:33 +0900

    4주차 자바 연습문제

--- jongmee/ch08_09.java
+++ jongmee/ch08_09.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ch08_09 {
+public class ch08_09 {
+  public static void main(String[] args) {
+    throw new UnsupportedFuctionException("지원하지 않는 기능입니다 .",100);
+  }
+
+}
+
+class UnsupportedFuctionException extends RuntimeException {
+  private final int ERR_CODE;
+
+  public UnsupportedFuctionException(String str, int code) {
+    super(str);
+    ERR_CODE = code;
+  }
+
+  public UnsupportedFuctionException(String str) {
+    this(str, 100);
+  }
+
+  public int getErrorCode(){
+    return ERR_CODE;
+  }
+  public String getMessage(){
+    return "["+getErrorCode()+"]"+super.getMessage();
+  }
 }
